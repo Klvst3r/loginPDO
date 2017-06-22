2fc4c24824178644f7b7dae82f813445b70569c7
klvst3r <klvst3r@gmail.com>
2017-06-22 14:26:12 -0500

    Validación de formulario con AJAX

--- vista/validarCode.php
+++ vista/validarCode.php
@@ -4,5 +4,27 @@
 
 include '../controlador/UsuarioControlador.php';
 
-// Ejecutamos el login del controlador por medio de la funcion estatica login se enviara un usuario y password
-echo UsuarioControlador::login("admin ","1234");
+//Vamos al final a obtener un resultado por AJAX
+$resultado = array();
+
+//Capurtamos los campos
+if(isset($_POST["txtUsuario"]) && isset($_POST["txtPassword"])){
+	// Ejecutamos el login del controlador por medio de la funcion estatica login se enviara un usuario y password
+	 
+	 $txtUsuario = $_POST["txtUsuario"];
+	 $txtPassword = $_POST["txtPassword"];
+
+	 $resultado = array("estado" => "true");
+	
+	//echo UsuarioControlador::login("admin","1234");
+	if(UsuarioControlador::login($txtUsuario, $txtPassword)){
+		//return print("Logeado");
+		//Retornamos los valor en JSON para que sea entendido por Javascript para su validación por AJAX
+		return print(json_encode($resultado));
+	}
+	
+}
+
+$resultado = array("estado" => "false");
+
+return print(json_encode($resultado));
